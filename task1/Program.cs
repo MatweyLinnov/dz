d5e91e90eb06ed6fa657e1bc567f77a400b023aa
Matwey <linnov1703@gmail.com>
2023-01-15 19:42:46 +0300

    Задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int number1 = new Random().Next(0, 10);
+int number2 = new Random().Next(0, 10);
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+
+int max = number1;
+
+if (number1 > number2)
+{
+   max = number1;
+}
+
+else 
+{
+    max = number2;
+}
+
+Console.Write("Max = ");
+Console.WriteLine(max);
+
+
