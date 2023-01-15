9702207f1d2539c54496a38966289a8691afabf8
Matwey <linnov1703@gmail.com>
2023-01-15 20:57:20 +0300

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int secondnumber = 1;
+
+Console.Write("Введите  число N:");
+int N = Int32.Parse(Console.ReadLine());
+//Console.Write(N);
+
+for(int i = 1; i<N; i++)
+{
+    if(i % 2==0) 
+    {
+        Console.Write( i + " ");
+    }
+}
