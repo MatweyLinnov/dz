b4a8d0a445f4f13823a128670f7c3cc567637dc1
Matwey <linnov1703@gmail.com>
2023-01-15 20:31:16 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите  число:");
+int number = Int32.Parse(Console.ReadLine());
+
+if (number % 2 ==1)
+{
+    Console.WriteLine("Число нечетное");
+}
+
+else 
+{
+    Console.WriteLine("Число четное");
+}
